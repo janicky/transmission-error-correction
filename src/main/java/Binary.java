5bc5b13a3981473c066d364fff48ad084dba68e7
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-15 21:02:41 +0100

    Add methods to binary class

--- src/main/java/Binary.java
+++ src/main/java/Binary.java
@@ -1,16 +1,35 @@
 public class Binary {
-    public static boolean[] getBooleanArray(char input) {
-        boolean[] word = new boolean[8];
-
-        int i = 0;
-        for (char b : Integer.toBinaryString(input).toCharArray()) {
-            word[i++] = (b == '1' ? true : false);
+    public static final String toString(boolean[] word) {
+        StringBuilder sb = new StringBuilder();
+        for (boolean b : word) {
+            sb.append(Binary.toInt(b));
         }
-
-        return word;
+        return sb.toString();
     }
 
     public static int toInt(boolean value) {
         return value ? 1 : 0;
+    }
+
+    public static int toInt(boolean[] word) {
+        int sum = 0, i = 0;
+        for (boolean b : word) {
+            sum += Math.pow(2, i);
+        }
+        return sum;
+    }
+
+    public static boolean toBoolean(int value) {
+        return value == '1' ? true : false;
+    }
+
+    public static boolean[] toBooleanArray(char input) {
+        boolean[] word = new boolean[8];
+
+        int i = 0;
+        for (char b : Integer.toBinaryString(input).toCharArray()) {
+            word[i++] = Binary.toBoolean(b);
+        }
+        return word;
     }
 }
