4b71176c69cdd4e3d0dd959aa54d51b9f4388e0a
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-15 10:21:08 +0100

    Add matrix constant to constants

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,2 +1,14 @@
 public class Constants {
+
+//    Hamming matrix
+    public static final boolean[][] MATRIX = new boolean[][]{
+            {false, false, false, true, true, false, true, true, true, false, false, false, false, false, false, false},
+            {false, false, true, true, false, true, false, true, false, true, false, false, false, false, false, false},
+            {false, true, true, false, true, false, false, true, false, false, true, false, false, false, false, false},
+            {false, true, false, true, false, true, true, false, false, false, false, true, false, false, false, false},
+            {true, false, true, false, true, false, true, false, false, false, false, false, true, false, false, false},
+            {true, true, false, false, true, true, false, false, false, false, false, false, false, true, false, false},
+            {true, true, true, true, false, false, false, false, false, false, false, false, false, false, true, false},
+            {true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, true}
+    };
 }
