496f1762258d4944c6f9827e2327b29c9698dae3
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-16 20:16:22 +0100

    Add binary class tests

--- src/test/java/BinaryTest.java
+++ src/test/java/BinaryTest.java
@@ -5,19 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinaryTest {
 
     @Test
-    void toInt() {
+    void toStringTest() {
+        boolean[] word = new boolean[] { false, false, true, true, false, true };
+        assertEquals("001101", Binary.toString(word));
+    }
+
+    @Test
+    void toIntTest() {
         assertEquals(2, Binary.toInt(new boolean[] { true, false }));
         assertEquals(4, Binary.toInt(new boolean[] { true, false, false }));
+        assertEquals(1, Binary.toInt(true));
+        assertEquals(0, Binary.toInt(false));
     }
 
     @Test
-    void toBoolean() {
+    void toBooleanTest() {
         assertEquals(false, Binary.toBoolean(0));
         assertEquals(true, Binary.toBoolean(1));
+        assertEquals(false, Binary.toBoolean('0'));
+        assertEquals(true, Binary.toBoolean('1'));
     }
 
     @Test
-    void toBooleanArray() {
+    void toBooleanArrayTest() {
         boolean[] expected = { false, true, false, false, false, false, false, true };
         boolean[] result = Binary.toBooleanArray('A');
 
