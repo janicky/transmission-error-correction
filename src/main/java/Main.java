7a37ea3e5286c0b22561dabe764852896a4ad4e3
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-19 00:18:42 +0100

    Move input file and add transitional file writer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,38 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.net.URL;
+import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        URL input_url = Main.class.getResource("input.txt");
-        File input_file = new File(input_url.getPath());
-        encodeInput(input_file);
+        File input_file = new File("data/input.txt");
+        File transitional_file = new File("data/transitional.txt");
+
+        encodeInput(input_file, transitional_file);
     }
 
-    public static void encodeInput(File file) {
+    public static void encodeInput(File input, File transitional) {
         try {
-            FileInputStream fis = new FileInputStream(file);
+            FileInputStream fis = new FileInputStream(input);
+//            Create file if doesn't exists
+            transitional.createNewFile();
+            FileOutputStream fos = new FileOutputStream(transitional);
+
+            Encoder encoder = new Encoder();
+
             while (fis.available() > 0) {
                 char word = (char) fis.read();
-                System.out.print(word);
+                boolean[] encoded = encoder.encode(word);
+                byte[] encoded_bytes = Binary.toString(encoded).getBytes();
+                fos.write(encoded_bytes);
+                fos.write('\n');
             }
+
+            System.out.println(transitional.getAbsolutePath());
+
+            fis.close();
+            fos.close();
         } catch (Exception e) {
-            System.out.println("Can't read input file.");
+            System.out.println("Can't open input or transitional file.");
         }
     }
 }
